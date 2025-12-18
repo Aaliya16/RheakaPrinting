fea404027b81ab51678cda2bcbb76a0c37bc6180
Nur Aaliya Maisarah binti Ishak <nuraaliya166@student.usm.my>
2025-12-19 02:08:18 +0800

    Setup DbConnection, create tables script, and updated header/footer

--- src/main/java/com/example/rheakaprinting/model/DbConnection.java
+++ src/main/java/com/example/rheakaprinting/model/DbConnection.java
@@ -1,4 +1,14 @@
 package com.example.rheakaprinting.model;
-
+import java.sql.*;
 public class DbConnection {
+    public static Connection getConnection() {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/rheaka_db","root","");
+            return con;
+        }catch(Exception e){
+            System.out.print(e);
+            return null;
+        }
+    }
 }
