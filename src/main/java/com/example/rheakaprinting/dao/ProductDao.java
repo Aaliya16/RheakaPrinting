45b80ff972ed1454e9714a475703d112aeadf809
aleaaqistina <aleaaqistina@gmail.com>
2025-12-19 15:10:06 +0800

    ADMIN PANEL

---
+++ src/main/java/com/example/rheakaprinting/dao/ProductDao.java
@@ -0,0 +1,6 @@
+package com.example.rheakaprinting.dao;
+
+import com.example.rheakaprinting.model.Product;
+
+public class ProductDao {
+}
---
+++ src/main/java/com/example/rheakaprinting/model/Product.java
@@ -0,0 +1,20 @@
+package com.example.rheakaprinting.model;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private int quantity;
+    private String description;
+    private String category;
+    private int stock;
+    public Product(int id, String name, double price, int quantity, String description, String category, int stock) {}
+}
+
+public Product(int id, String name, double price, int quantity, String description, String category, int stock) {
+    n = name;
+    d= description;
+    p = price;
+    q = quantity;
+
+}
